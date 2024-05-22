4bd5a3bfc9a8cc3a9c1e2ef22678b8d0dc959711
Viki Val <viktorko99@gmail.com>
2024-05-22 15:00:46 +0200

    :squid: codegen

--- src/model/generated/collectionEntity.model.ts
+++ src/model/generated/collectionEntity.model.ts
@@ -1,93 +1,104 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, BigIntColumn as BigIntColumn_, DateTimeColumn as DateTimeColumn_, StringColumn as StringColumn_, IntColumn as IntColumn_, OneToMany as OneToMany_, Index as Index_, ManyToOne as ManyToOne_} from "@subsquid/typeorm-store"
-import {CollectionEvent} from "./collectionEvent.model"
-import {MetadataEntity} from "./metadataEntity.model"
-import {NFTEntity} from "./nftEntity.model"
-import {CollectionType} from "./_collectionType"
+import {
+  BigIntColumn as BigIntColumn_,
+  Column as Column_,
+  DateTimeColumn as DateTimeColumn_,
+  Entity as Entity_,
+  Index as Index_,
+  IntColumn as IntColumn_,
+  ManyToOne as ManyToOne_,
+  OneToMany as OneToMany_,
+  PrimaryColumn as PrimaryColumn_,
+  StringColumn as StringColumn_,
+} from '@subsquid/typeorm-store'
+import { CollectionEvent } from './collectionEvent.model'
+import { MetadataEntity } from './metadataEntity.model'
+import { NFTEntity } from './nftEntity.model'
+import { CollectionType } from './_collectionType'
 
 @Entity_()
 export class CollectionEntity {
-    constructor(props?: Partial<CollectionEntity>) {
-        Object.assign(this, props)
-    }
+  constructor(props?: Partial<CollectionEntity>) {
+    Object.assign(this, props)
+  }
 
-    @BigIntColumn_({nullable: true})
-    blockNumber!: bigint | undefined | null
+  @BigIntColumn_({ nullable: true })
+  blockNumber!: bigint | undefined | null
 
-    @DateTimeColumn_({nullable: false})
-    createdAt!: Date
+  @DateTimeColumn_({ nullable: false })
+  createdAt!: Date
 
-    @StringColumn_({nullable: false})
-    currentOwner!: string
+  @StringColumn_({ nullable: false })
+  currentOwner!: string
 
-    @IntColumn_({nullable: false})
-    distribution!: number
+  @IntColumn_({ nullable: false })
+  distribution!: number
 
-    @OneToMany_(() => CollectionEvent, e => e.collection)
-    events!: CollectionEvent[]
+  @OneToMany_(() => CollectionEvent, (e) => e.collection)
+  events!: CollectionEvent[]
 
-    @BigIntColumn_({nullable: false})
-    floor!: bigint
+  @BigIntColumn_({ nullable: false })
+  floor!: bigint
 
-    @Index_({unique: true})
-    @StringColumn_({nullable: false})
-    hash!: string
+  @Index_({ unique: true })
+  @StringColumn_({ nullable: false })
+  hash!: string
 
-    @BigIntColumn_({nullable: false})
-    highestSale!: bigint
+  @BigIntColumn_({ nullable: false })
+  highestSale!: bigint
 
-    @PrimaryColumn_()
-    id!: string
+  @PrimaryColumn_()
+  id!: string
 
-    @StringColumn_({nullable: true})
-    image!: string | undefined | null
+  @StringColumn_({ nullable: true })
+  image!: string | undefined | null
 
-    @StringColumn_({nullable: false})
-    issuer!: string
+  @StringColumn_({ nullable: false })
+  issuer!: string
 
-    @IntColumn_({nullable: true})
-    max!: number | undefined | null
+  @IntColumn_({ nullable: true })
+  max!: number | undefined | null
 
-    @StringColumn_({nullable: true})
-    media!: string | undefined | null
+  @StringColumn_({ nullable: true })
+  media!: string | undefined | null
 
-    @Index_()
-    @ManyToOne_(() => MetadataEntity, {nullable: true})
-    meta!: MetadataEntity | undefined | null
+  @Index_()
+  @ManyToOne_(() => MetadataEntity, { nullable: true })
+  meta!: MetadataEntity | undefined | null
 
-    @StringColumn_({nullable: true})
-    metadata!: string | undefined | null
+  @StringColumn_({ nullable: true })
+  metadata!: string | undefined | null
 
-    @Index_()
-    @StringColumn_({nullable: true})
-    name!: string | undefined | null
+  @Index_()
+  @StringColumn_({ nullable: true })
+  name!: string | undefined | null
 
-    @IntColumn_({nullable: false})
-    nftCount!: number
+  @IntColumn_({ nullable: false })
+  nftCount!: number
 
-    @OneToMany_(() => NFTEntity, e => e.collection)
-    nfts!: NFTEntity[]
+  @OneToMany_(() => NFTEntity, (e) => e.collection)
+  nfts!: NFTEntity[]
 
-    @IntColumn_({nullable: false})
-    ownerCount!: number
+  @IntColumn_({ nullable: false })
+  ownerCount!: number
 
-    @IntColumn_({nullable: false})
-    supply!: number
+  @IntColumn_({ nullable: false })
+  supply!: number
 
-    @StringColumn_({nullable: true})
-    baseUri!: string | undefined | null
+  @StringColumn_({ nullable: true })
+  baseUri!: string | undefined | null
 
-    @StringColumn_({nullable: true})
-    symbol!: string | undefined | null
+  @StringColumn_({ nullable: true })
+  symbol!: string | undefined | null
 
-    @Column_("varchar", {length: 7, nullable: false})
-    type!: CollectionType
+  @Column_('varchar', { length: 7, nullable: false })
+  type!: CollectionType
 
-    @DateTimeColumn_({nullable: false})
-    updatedAt!: Date
+  @DateTimeColumn_({ nullable: false })
+  updatedAt!: Date
 
-    @IntColumn_({nullable: false})
-    version!: number
+  @IntColumn_({ nullable: false })
+  version!: number
 
-    @BigIntColumn_({nullable: false})
-    volume!: bigint
+  @BigIntColumn_({ nullable: false })
+  volume!: bigint
 }
--- src/model/generated/event.model.ts
+++ src/model/generated/event.model.ts
@@ -1,35 +1,44 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, BigIntColumn as BigIntColumn_, DateTimeColumn as DateTimeColumn_, StringColumn as StringColumn_, ManyToOne as ManyToOne_, Index as Index_} from "@subsquid/typeorm-store"
-import {Interaction} from "./_interaction"
-import {NFTEntity} from "./nftEntity.model"
+import {
+  BigIntColumn as BigIntColumn_,
+  Column as Column_,
+  DateTimeColumn as DateTimeColumn_,
+  Entity as Entity_,
+  Index as Index_,
+  ManyToOne as ManyToOne_,
+  PrimaryColumn as PrimaryColumn_,
+  StringColumn as StringColumn_,
+} from '@subsquid/typeorm-store'
+import { Interaction } from './_interaction'
+import { NFTEntity } from './nftEntity.model'
 
 @Entity_()
 export class Event {
-    constructor(props?: Partial<Event>) {
-        Object.assign(this, props)
-    }
+  constructor(props?: Partial<Event>) {
+    Object.assign(this, props)
+  }
 
-    @PrimaryColumn_()
-    id!: string
+  @PrimaryColumn_()
+  id!: string
 
-    @BigIntColumn_({nullable: true})
-    blockNumber!: bigint | undefined | null
+  @BigIntColumn_({ nullable: true })
+  blockNumber!: bigint | undefined | null
 
-    @DateTimeColumn_({nullable: false})
-    timestamp!: Date
+  @DateTimeColumn_({ nullable: false })
+  timestamp!: Date
 
-    @StringColumn_({nullable: false})
-    caller!: string
+  @StringColumn_({ nullable: false })
+  caller!: string
 
-    @StringColumn_({nullable: false})
-    currentOwner!: string
+  @StringColumn_({ nullable: false })
+  currentOwner!: string
 
-    @Column_("varchar", {length: 6, nullable: false})
-    interaction!: Interaction
+  @Column_('varchar', { length: 6, nullable: false })
+  interaction!: Interaction
 
-    @StringColumn_({nullable: false})
-    meta!: string
+  @StringColumn_({ nullable: false })
+  meta!: string
 
-    @Index_()
-    @ManyToOne_(() => NFTEntity, {nullable: true})
-    nft!: NFTEntity
+  @Index_()
+  @ManyToOne_(() => NFTEntity, { nullable: true })
+  nft!: NFTEntity
 }
